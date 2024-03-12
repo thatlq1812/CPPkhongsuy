a7b346343fa6a2fa8d2c0cfa04bf393c411d4fa2
Le Quang That <fxlqthat@gmail.com>
2024-03-12 23:29:30 +0700

    Refactor code to improve performance and readability

---
+++ 001 - Begin/Test04.cpp
@@ -0,0 +1,17 @@
+// Generate 01 program for beginer
+// Test04.cpp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Sum = " << a + b << endl;
+    return 0;
+}
+
+// Output
+// Enter two numbers: 10 20
+// Sum = 30
