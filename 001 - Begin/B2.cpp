c5ceb9ed77aabdc2add221c9e458df2b90d7eda1
nguoidepdangsuy <163025020+nguoidepdangsuy@users.noreply.github.com>
2024-03-13 00:27:12 +0700

    Khoc huhaha

---
+++ 001 - Begin/B2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    char tenSV[30];
+    cout << "Y/c nhap ten: ";
+    cin.getline (tenSV,30);
+   
+    cout << "Ten sv la: " << tenSV << endl;
+    system("pause");
+    return 0;
+}
---
+++ 001 - Begin/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+   char tenSV;
+   cout << "Y/c nhap ten";
+   
+
+    system("pause");
+    return 0;
+}
